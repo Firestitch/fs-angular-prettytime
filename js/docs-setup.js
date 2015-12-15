d3bc52cf129e8e8c99de935fd54d9d8319233200
Raymond <ray@firestitch.com>
2015-12-15 16:46:00 -0500

    updated

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -5,21 +5,21 @@ NG_DOCS={
   "pages": [
     {
       "section": "api",
-      "id": "app.controllers:ListerCtrl",
-      "shortName": "ListerCtrl",
-      "type": "interface",
-      "moduleName": "app",
-      "shortDescription": "A factory that allows easy access to the lister directive instance",
-      "keywords": "access allows api app controllers data directive easy factory instance interface lister load loading method triggers"
-    },
-    {
-      "section": "api",
-      "id": "app.directives:lister",
-      "shortName": "lister",
+      "id": "app.directives:prettytime",
+      "shortName": "prettytime",
       "type": "directive",
       "moduleName": "app",
       "shortDescription": "",
-      "keywords": "$scope __all access actions api app appened applies array avoid binded box button call callback called class classname click clicked column columns configure contextual corresponds css custom data dataset default directive directives dropdown element entire equal events expecting fetch filter filterable filters function functions greater href html icon input interface item js key label length limit list lister listerinstance load ls-instance ls-options menu number object objects options paging paired parameter parameters passed perform places populate process properties query record records rendering ror row select selection sets side specifies starting supports table table-bordered table-striped text tile title total triggered type type-hint type-hint-array type-hint-function user values variables-matrix"
+      "keywords": "api app directive directives"
+    },
+    {
+      "section": "api",
+      "id": "app.services:prettytimeService",
+      "shortName": "prettytimeService",
+      "type": "interface",
+      "moduleName": "app",
+      "shortDescription": "",
+      "keywords": "accepts api app days difference format formated interface method pretty return services time version"
     }
   ],
   "apis": {
